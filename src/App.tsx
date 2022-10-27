dd8ea458cf258418e775a9ebe975777c172393ea
Emille Junior <emille.junior@gmail.com>
2022-10-27 17:50:03 +0700

    implement basic redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,59 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment, decrement } from './app/features/counter/counterSlice'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // const [count, setCount] = useState(0)
+  const [localCount, setLocalCount] = useState(0)
+  const count = useSelector((state)=>state.counter.count)
+  const dispatch = useDispatch()
+
+  const incrementCounter = ()=>{
+    console.log("The Increment Button is Clicked")
+
+    console.log(increment())
+    // this increment function will return an object with two properties
+    // the type of the action which is a string, and the payload
+    
+    // That object is what we need to send to the store to modify our state
+    dispatch(increment())
+
+    // This works too
+    // dispatch({
+    //   type: "counter/increment"
+    // })
+  }
+
+  const decrementCounter = ()=>{
+    console.log("The Decrement Button is Clicked")
+    dispatch(decrement())
+  }
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src="/vite.svg" className="logo" alt="Vite logo" />
+        <img src={reactLogo} className="logo react" alt="React logo" />
+        <img src="https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg" className="logo redux" alt="Redux logo" />
       </div>
-      <h1>Vite + React</h1>
+
+      <h1>Vite + React + Redux (RTK)</h1>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <p>count is <b>{count}</b></p>
+        {/* <button onClick={() => setCount((count) => count + 1)}> */}
+        <button onClick={incrementCounter}>
+          {`Increment (+1)`}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {/* <button onClick={() => setCount((count) => count - 1)}> */}
+        <button onClick={decrementCounter} style={{marginLeft: '8px'}}>
+          {`Decrement (-1)`}
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </div>
   )
 }
